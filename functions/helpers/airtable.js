ad09c1f999eaa1017b22372b85e6fd0ee862ff89
Pierre Lingat <northernstargazer@192-168-1-9.tpgi.com.au>
2020-11-29 01:15:09 +1100

    Fixed things

--- functions/helpers/airtable.js
+++ functions/helpers/airtable.js
@@ -1,7 +1,8 @@
-// TODO: create Airtable configuration
-require("dotenv").config(); 
-const Airtable = require("airtable");
-const base = new Airtable({apiKey: "keyrY4bCSA8vsHJHp"}).base("app6p8u38rAt91Xxm");
-const table = base("courses");
+require('dotenv').config();
+var Airtable = require('airtable');
+var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+    process.env.AIRTABLE_BASE_ID
+);
+const table = base(process.env.AIRTABLE_TABLE_NAME);
 
 module.exports = { table };
